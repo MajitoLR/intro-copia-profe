51129a5ce069cf94efd45fa5b7d4c8984624696a
MajitoLR <larreamariajose456@gmail.com>
2026-02-26 13:43:42 -0500

    Update Pagina2.py

--- pages/Pagina2.py
+++ pages/Pagina2.py
@@ -4,8 +4,7 @@ from PIL import Image
 st.title("Mi página 2")
 
 st.header("Esta es mi página de presentación")
-image= Image.open ("imagen mariposa.jpg")
-st.image(image,caption= "Esta es mi imagen")
+
 texto= st.text_input ("Ingresa texto","texto inicial")
 st.write("El texto que escribiste es",texto)
 if st.button("Presiona el botón"):
