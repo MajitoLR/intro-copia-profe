508301a01816b24ccfd389206cbe11c44c26db58
MajitoLR <larreamariajose456@gmail.com>
2026-02-26 13:43:05 -0500

    Update Pagina2.py

--- pages/Pagina2.py
+++ pages/Pagina2.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Mi página 2")
+
+st.header("Esta es mi página de presentación")
+image= Image.open ("imagen mariposa.jpg")
+st.image(image,caption= "Esta es mi imagen")
+texto= st.text_input ("Ingresa texto","texto inicial")
+st.write("El texto que escribiste es",texto)
+if st.button("Presiona el botón"):
+  st.write("Presonaste el botón")
